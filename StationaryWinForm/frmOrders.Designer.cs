c68d5f4bb61958c3614bab04acf1b342b252e003
Adam <Adam-Nicholls@live.nmit.ac.nz>
2018-06-25 13:32:18 +1200

    labels

--- StationaryWinForm/frmOrders.Designer.cs
+++ StationaryWinForm/frmOrders.Designer.cs
@@ -31,10 +31,10 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnDelete = new System.Windows.Forms.Button();
             this.lstOrders = new System.Windows.Forms.ListBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.rtbDetails = new System.Windows.Forms.RichTextBox();
             this.lblTotalIncome = new System.Windows.Forms.Label();
             this.btnClose = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -48,7 +48,7 @@
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(374, 22);
+            this.btnDelete.Location = new System.Drawing.Point(338, 25);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(75, 23);
             this.btnDelete.TabIndex = 1;
@@ -60,27 +60,18 @@
             this.lstOrders.FormattingEnabled = true;
             this.lstOrders.Location = new System.Drawing.Point(12, 25);
             this.lstOrders.Name = "lstOrders";
-            this.lstOrders.Size = new System.Drawing.Size(120, 147);
+            this.lstOrders.Size = new System.Drawing.Size(79, 147);
             this.lstOrders.TabIndex = 2;
             this.lstOrders.SelectedIndexChanged += new System.EventHandler(this.lstOrders_SelectedIndexChanged);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(68, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(31, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Price";
             // 
             // rtbDetails
             // 
             this.rtbDetails.BackColor = System.Drawing.SystemColors.Window;
             this.rtbDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rtbDetails.Location = new System.Drawing.Point(138, 25);
+            this.rtbDetails.Location = new System.Drawing.Point(97, 25);
             this.rtbDetails.Name = "rtbDetails";
             this.rtbDetails.ReadOnly = true;
-            this.rtbDetails.Size = new System.Drawing.Size(230, 147);
+            this.rtbDetails.Size = new System.Drawing.Size(235, 147);
             this.rtbDetails.TabIndex = 4;
             this.rtbDetails.Text = "";
             // 
@@ -95,7 +86,7 @@
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(374, 169);
+            this.btnClose.Location = new System.Drawing.Point(338, 172);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(75, 23);
             this.btnClose.TabIndex = 6;
@@ -103,15 +94,24 @@
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Order Info";
+            // 
             // frmOrders
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(461, 203);
+            this.ClientSize = new System.Drawing.Size(418, 203);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.lblTotalIncome);
             this.Controls.Add(this.rtbDetails);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.lstOrders);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.label1);
@@ -128,9 +128,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.ListBox lstOrders;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.RichTextBox rtbDetails;
         private System.Windows.Forms.Label lblTotalIncome;
         private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label2;
     }
 }
